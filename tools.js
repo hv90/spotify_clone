a661103ac9db14054583c7aa20761afda1442755
Hugo <hugomachado182@gmail.com>
2019-08-12 03:11:08 -0300

    adicionada ferramenta para identificar faixas repetidas em pl's

--- tools.js
+++ tools.js
@@ -1,45 +1,58 @@
 
-module.exports = {
-    get_pl_info: function (reference, pl_number){
-        let file = require(reference)
-        var id = file["items"][pl_number]["id"]//req.query
-        var name = file["items"][pl_number]["name"]
-    
-        return {id, name}
-    },
 
-    get_multi_pl_info: function (reference, destiny, start, end){
-        var info = {"info": []} 
-        var start = start
-        var end = end
-      
-        fs.readFile(reference, (err, data)=>{
-          if(err) throw err
-          var data = JSON.parse(data)
-      
-          for(var i = start; i < end; i++){
-            var pl_name = data["items"][i]["name"]
-            var uri = data["items"][i]["id"]
-            info["info"].push({pl_name, uri })  
-          }
-      
-          fs.writeFile(destiny, JSON.stringify(info), err =>{
-            if(err) throw err
-          }) 
-        })
+function get_pl_info (pl_source, pl_number){
+  let file = require(pl_source)
+  var id = file["items"][pl_number]["id"]//req.query
+  var name = file["items"][pl_number]["name"]
+  
+  return {id, name}
+}
+
+function get_multi_pl_info (reference, destiny, start, end){
+  var info = {"info": []} 
+  var start = start
+  var end = end
+
+  fs.readFile(reference, (err, data)=>{
+    if(err) throw err
+    var data = JSON.parse(data)
+
+    for(var i = start; i < end; i++){
+      var pl_name = data["items"][i]["name"]
+      var uri = data["items"][i]["id"]
+      info["info"].push({pl_name, uri })  
     }
+
+    fs.writeFile(destiny, JSON.stringify(info), err =>{
+      if(err) throw err
+    }) 
+  })
 }
 
-function repeatedFilesFinder(){
-  var reference = require("./playlists/pumpkins.json")
-  
-  for (var i = 0; i < reference["info"].length; i++){
-    console.log(reference["info"][i].pl_name)
-  } 
+function repeatedFilesFinder(path1, path2){
+  var A = require(path1)
+  var B = require(path2)
+  var result = []
+
+
+  //console.log(A["items"][1]["track"]["uri"])
+  for (var i = 0; i < A["items"].length; i++){
+    for( var j = 0; j < B["items"].length; j++){
+      if(A["items"][i]["track"]["id"] == B["items"][j]["track"]["id"]){
+        data = {
+          "uri": A["items"][j]["track"]["uri"],
+          "positions": [j]
+        }
+        
+        result.push(data)
+      }
+    }
+  }
+  return result
 }
 
-repeatedFilesFinder()
+
+//repeatedFilesFinder(".//playlists//Blue Pumpkins.json", ".//playlists//Purple Pumpkins.json")
   
   
-  
-  
+module.exports = {get_pl_info, get_multi_pl_info, repeatedFilesFinder}
